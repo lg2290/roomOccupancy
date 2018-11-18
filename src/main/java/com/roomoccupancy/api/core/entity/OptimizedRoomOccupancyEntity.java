94d9d0c1e75276068da59d21a778929f8292949a
Luis Gustavo Garcia de Carvalho <lg2290@DESKTOP-GOIOK3N.localdomain>
2018-11-18 16:41:42 +0000

    Addtion of constructor and getters on the entities

--- src/main/java/com/roomoccupancy/api/core/entity/OptimizedRoomOccupancyEntity.java
+++ src/main/java/com/roomoccupancy/api/core/entity/OptimizedRoomOccupancyEntity.java
@@ -1,10 +1,5 @@
 package com.roomoccupancy.api.core.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * Contains informations about the rooms' optimized division - how many Premium
  * and Economy rooms will be occupied, and how much money each category will
@@ -13,14 +8,24 @@ import lombok.NoArgsConstructor;
  * @author luis
  *
  */
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class OptimizedRoomOccupancyEntity {
 
 	private RoomCategoryOccupancyEntity premiumOccupancy;
 
 	private RoomCategoryOccupancyEntity economyOccupancy;
+
+	public OptimizedRoomOccupancyEntity(RoomCategoryOccupancyEntity premiumOccupancy,
+			RoomCategoryOccupancyEntity economyOccupancy) {
+		this.premiumOccupancy = premiumOccupancy;
+		this.economyOccupancy = economyOccupancy;
+	}
+
+	public RoomCategoryOccupancyEntity getPremiumOccupancy() {
+		return premiumOccupancy;
+	}
+
+	public RoomCategoryOccupancyEntity getEconomyOccupancy() {
+		return economyOccupancy;
+	}
 
 }
--- src/main/java/com/roomoccupancy/api/core/entity/RoomCategoryOccupancyEntity.java
+++ src/main/java/com/roomoccupancy/api/core/entity/RoomCategoryOccupancyEntity.java
@@ -1,10 +1,5 @@
 package com.roomoccupancy.api.core.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * Contains informations of how much rooms will be occupied, and how much money
  * the reservations will yield
@@ -12,13 +7,22 @@ import lombok.NoArgsConstructor;
  * @author luis
  *
  */
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class RoomCategoryOccupancyEntity {
 
 	private Integer numberOfOccupiedRooms;
 
 	private Integer generatedIncome;
+
+	public RoomCategoryOccupancyEntity(Integer numberOfOccupiedRooms, Integer generatedIncome) {
+		this.generatedIncome = generatedIncome;
+		this.numberOfOccupiedRooms = numberOfOccupiedRooms;
+	}
+
+	public Integer getNumberOfOccupiedRooms() {
+		return numberOfOccupiedRooms;
+	}
+
+	public Integer getGeneratedIncome() {
+		return generatedIncome;
+	}
 }
