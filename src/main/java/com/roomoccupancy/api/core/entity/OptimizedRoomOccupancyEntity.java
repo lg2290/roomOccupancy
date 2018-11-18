a6a620de87244cb162098a32b1507a893cf01332
Luis Gustavo Garcia de Carvalho <lg2290@DESKTOP-GOIOK3N.localdomain>
2018-11-18 15:11:22 +0000

    Creation of the core entities that will be returned by the room occupancy usecase

---
+++ src/main/java/com/roomoccupancy/api/core/entity/OptimizedRoomOccupancyEntity.java
@@ -0,0 +1,26 @@
+package com.roomoccupancy.api.core.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Contains informations about the rooms' optimized division - how many Premium
+ * and Economy rooms will be occupied, and how much money each category will
+ * generate.
+ * 
+ * @author luis
+ *
+ */
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OptimizedRoomOccupancyEntity {
+
+	private RoomCategoryOccupancy premiumOccupancy;
+
+	private RoomCategoryOccupancy economyOccupancy;
+
+}
---
+++ src/main/java/com/roomoccupancy/api/core/entity/RoomCategoryOccupancy.java
@@ -0,0 +1,24 @@
+package com.roomoccupancy.api.core.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Contains informations of how much rooms will be occupied, and how much money
+ * the reservations will yield
+ * 
+ * @author luis
+ *
+ */
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomCategoryOccupancy {
+
+	private Integer numberOfOccupiedRooms;
+
+	private Integer generatedIncome;
+}
